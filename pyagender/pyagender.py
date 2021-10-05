ff74b7390f3d689347f77e618ef31b75b0daa0fe
featuring-poza <poza@featuring.in>
2021-10-05 18:59:12 +0900

    python 3.8 Support, keras 2.6 Support

--- pyagender/pyagender.py
+++ pyagender/pyagender.py
@@ -7,7 +7,7 @@ import numpy as np
 sys.setrecursionlimit(2 ** 20)
 # np.random.seed(2 ** 10)
 
-from image.py_agender.pyagender.wide_resnet import WideResNet
+from .wide_resnet import WideResNet
 
 
 class PyAgender():
